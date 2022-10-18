2a793e7bb5dfe98ab4c51b46809a49f8cd7b5d53
AdamMusluev <player.300@mail.ru>
2022-10-18 11:12:25 +0300

    added button and used some styles

--- screens/Scanner.js
+++ screens/Scanner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View, Button, Linking } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
+import BarcodeMask from "react-native-barcode-mask";
 
 const Scanner = () => {
   const [hasPermission, setHasPermission] = useState(null);
@@ -24,19 +25,36 @@ const Scanner = () => {
 
   return (
     <View style={styles.container}>
+      <View>
+        <Text>ADAM</Text>
+      </View>
       <BarCodeScanner
+  
         onBarCodeScanned={scanned ? undefined : handleSuccess}
         style={StyleSheet.absoluteFillObject}
-      />
-      {scanned && <Button title="tap to Scan again" onPress={() => setScanned(false)} />}
+        />
+      <BarcodeMask edgeColor="#fff" showAnimatedLine width={350} height={150} outerMaskOpacity={0.8} />
+        {scanned && (
+          <View style={styles.buttonContainer}> 
+            <Button style={styles.button_1} title="tap to Scan again" onPress={() => setScanned(false)} />
+          </View>
+        )}
     </View>
   );
-}
+};
 export default Scanner;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
   },
+  buttonContainer: {
+    alignItems: 'center',
+    width: 350,
+    marginTop: 100,
+  },
+  button_1: {
+    padding: 20,
+    color: '#000'
+  }
 });
